e448e854191bf691ffefaba7a28c3de9577ba7c0
philip-roy-jones <jones.philipr@gmail.com>
2024-09-05 21:08:08 -0400

    Frontend modifications for deployment

--- React UI/vite.config.js
+++ React UI/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/exercises': {
-        target: 'http://localhost:3000'
+        target: 'https://exercise-tracker-backend-5nzw.onrender.com'
       }
     }
   }
